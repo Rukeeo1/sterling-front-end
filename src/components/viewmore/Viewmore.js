ecc72bdadbe5d73af2ccdd3b1d3813fb88753c43
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-03 22:00:18 +0100

    added viewmore component

---
+++ src/components/viewmore/Viewmore.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ViewMore(){
+  return (
+    <h2 className="btn btn-primary d-flex justify-content-center mt-5" style={{width:"450px", margin:"auto", background:"white"}}>VIEW MORE</h2>
+  )
+}
+
+export default ViewMore;
