bfbdb8a35cadb319ce1a876b5c8cb44abb1dcc8b
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-04 13:29:07 +0100

    adde conditional rendering to view more and viewmore to starship cards

--- src/components/viewmore/Viewmore.js
+++ src/components/viewmore/Viewmore.js
@@ -3,9 +3,10 @@ import {Link } from 'react-router-dom'
 
 
 function ViewMore(props){
+
   return (
     <Link to={`/${props.page}`}>
-    <h2 className="btn btn-primary d-flex justify-content-center mt-5" style={{width:"450px", margin:"auto", background:"white"}}>VIEW MORE</h2>
+    <h2 className="btn btn-primary d-flex justify-content-center mt-5" style={{width:"450px", margin:"auto", background:"white"}}>{props.tag? props.tag : `VIEW MORE`}</h2>
     </Link>
   )
 }
