49c09c36a17c608e2b3914e31dd58ed33ffd56d3
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-04 01:09:38 +0100

    passing names as props to character card

--- src/components/card/CharacterCard.js
+++ src/components/card/CharacterCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 function CharacterCard(props) {
   return (
     <div class="card shadow" style={{ width: "36rem" }}>
@@ -9,8 +10,8 @@ function CharacterCard(props) {
         </div>
         <div class="col p-0 py-4">
           <div class="card-block px-3">
-            <h4 class="card-title">Lorem ipsum dolor sit amet</h4>
-            <p class="card-text">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <span className="character-readmore-tag">...ReadMore</span> </p>
+            <h4 class="card-title">{props.name}</h4>
+            <p class="card-text">{`${props.name}, a force sensitive human male, was a legendary Jedi Master who fought in the Galatic Civil War during the reign of tje Galatic Empire`} <span className="character-readmore-tag">...ReadMore</span> </p>
           </div>
         </div>
       </div>
