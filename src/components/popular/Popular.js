9cf2ed3d274e4597823eae2ebe25e91d40e2712c
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-04 01:12:38 +0100

    passing name as props to popular.js

--- src/components/popular/Popular.js
+++ src/components/popular/Popular.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Hr from '../underline/Underline'
 
-function Popular(){
+function Popular(props){
   return(
     <div style={{textAlign:"center", marginBottom:"0px", fontWeight:"bold", fontSize:"1.6em"}} className="topic-tag">
-  <h3 style={{marginBottom:"0px", color:"#4c4c58"}}>Popular Starships</h3>
+  <h3 style={{marginBottom:"0px", color:"#4c4c58"}}>{props.subject}</h3>
   <Hr width="3.5em" marginTop="5px" height="5px" background="#494956"/>
 </div>
   )
