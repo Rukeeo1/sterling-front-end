8eb2ba61967db9f997a6ff57037bbb7b8688bc67
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-04 01:51:44 +0100

    character component that renders the characters on the homepage

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,18 +1,33 @@
-import React from 'react';
-import Card from '../card/Card'
+import React, {useEffect, useState} from 'react';
 import Luke from '../../assests/character-1.jpg'
+import CThirty from '../../assests/character-2.jpg'
 import CharacterCard from '../card/CharacterCard'
+import axios from 'axios'
 import './css/Characters.css'
 
+const pictures = [Luke,CThirty, Luke, CThirty]
+
+
 function Characters() {
-  //if homepage render four...and hide pagination
+  const [characters, setUseCharacters] = useState()
+  useEffect(()=> {
+    axios.get('https://swapi.co/api/people/').then(res=> {
+      const characters = res.data.results.slice(0,4) 
+      setUseCharacters(characters)
+    }).catch(err=> {
+      console.log(err)
+    })
+  },[])
+
+  if(!characters) return ''
 
   return (
     <section className="d-flex !important justify-content-center flex-wrap ">
-     <CharacterCard  imageSource={Luke}/>
-     <CharacterCard  imageSource={Luke}/>
-     <CharacterCard  imageSource={Luke}/>
-     <CharacterCard  imageSource={Luke}/>
+     {
+       characters.map((character, index)=>{
+         return   <CharacterCard key={index} name={character.name} imageSource={pictures[index]}/>
+       })
+     }
     </section>
 
   )
