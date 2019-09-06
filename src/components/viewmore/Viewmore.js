89c7b903db92297dd9065b7ec91eb73813910e76
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-06 09:13:28 +0100

    added responsiveness to viewmore

--- src/components/viewmore/Viewmore.js
+++ src/components/viewmore/Viewmore.js
@@ -6,7 +6,7 @@ function ViewMore(props){
 
   return (
     <Link to={`/${props.page}`}>
-    <h2 className="btn btn-primary d-flex justify-content-center mt-5" style={{width:"450px", margin:"auto", background:"white"}}>{props.tag? props.tag : `VIEW MORE`}</h2>
+    <h2 className="btn btn-primary w-25 d-flex justify-content-center mt-5" style={{margin:"auto", background:"white"}}>{props.tag? props.tag : `VIEW MORE`}</h2>
     </Link>
   )
 }
