47375d5c3ee63239a951caafa9160bbc64c73fda
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-06 18:31:16 +0100

    import images

--- src/components/starships/Starships.js
+++ src/components/starships/Starships.js
@@ -1,21 +1,28 @@
 import React, {useEffect, useState} from 'react'
 import starship1 from '../../assests/starship-1.jpg'
-import ButtonGroup from '../buttonGroups/ButtonGroups'
+import starship2 from '../../assests/starship-2.jpg'
+import starship3 from '../../assests/starship-3.jpg'
+import starship4 from '../../assests/starship-4.jpg'
+import starship5 from '../../assests/starship-5.jpg'
+import starship6 from '../../assests/starship-6.jpg'
+import SelectTagInput from '../selectTagInput/SelectTagInput'
 import Spinner from '../spinner/Spinner'
 import './css/Starship.css'
 import Card from '../card/Card.js'
 import axios from 'axios';
+
+const starshipImages = [starship1,  starship2,  starship3 , starship4,starship5, starship6]
+
+const generateRandom = () => Math.floor(Math.random() * 5) + 1  
 
 
 
 function Starships(){
   const [starships, setStarships] = useState('');
   const [loading, setLoading] = useState(false)
-  console.log(starships,'these are')
 
   useEffect(()=> {
     setLoading(true)
-
     axios.get('https://swapi.co/api/starships/').then(res => {
       console.log('response', res)
       const ships = res.data.results.slice(0,6) 
@@ -26,6 +33,8 @@ function Starships(){
       console.log(err)
     })
   },[])
+  
+  console.log(starships,'hello rukee')
 
   if(loading) return <Spinner />
 
@@ -37,7 +46,7 @@ function Starships(){
       {
         starships.map((ship, index) => {
           return (
-            <Card key={index} wrappingDiv="card shadow" objectName={ship.name} imageSource={starship1} description={`The ${ship.name} is a modified ${ship.model} light weight manufactured by the ${ship.manufacturer}`} readmorestyle="space-ship-readmore" url={ship.url} style={{marginTop:"20px"}}/>
+            <Card key={index} wrappingDiv="card shadow" cargoCapacity={ship.cargo_capacity} objectName={ship.name} imageSource={starshipImages[generateRandom()]} description={`The ${ship.name} is a modified ${ship.model} light weight and has a cargo capacity of ${ship.cargo_capacity}`} readmorestyle="space-ship-readmore" url={ship.url} style={{marginTop:"20px"}}/>
           )
         })
       }
