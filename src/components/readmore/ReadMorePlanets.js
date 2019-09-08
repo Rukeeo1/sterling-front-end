627bc49354d76a28f7f9ab27f13b767d796942ea
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-08 11:36:07 +0100

    fetching planet data from api

--- src/components/readmore/ReadMorePlanets.js
+++ src/components/readmore/ReadMorePlanets.js
@@ -1,24 +1,48 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import ReadMorePageHeader from './ReadMorePageHeader'
 import { planets } from './ReadMorePageHeader'
-console.log(planets,'hello')
+import axios from 'axios'
+console.log(planets, 'hello')
 
 
 
-function ReadMorePlanets(){
+function ReadMorePlanets({match}) {
   const [planet, setPlanet] = useState('')
-  // const planetId = match.params['id'];
-return (
-  <>
-  <ReadMorePageHeader page={planets} hello="rukee"/>
-  <h1>hello rukee how are you doing</h1>
-  <div className="container">
-    <img src={planets[0]}/>
-    <p></p>
-    
-  </div>
-  </>
-)
+  const planetId = match.params['id'];
+
+  useEffect(()=>{
+    axios.get(`https://swapi.co/api/planets/${planetId}`).then(response => {
+      setPlanet(response.data)
+    }).catch(err=>{
+      console.log(err)
+    })
+  },[])
+
+  return (
+    <>
+      <ReadMorePageHeader page={planets} hello="rukee" />
+
+
+      <div className="container">
+        <h2 className="mt-5 mb-5">{planet.name}</h2>
+        <div className="d-flex">
+          <img src={planets[0]} style={{ height: "350px" }} />
+          <div>
+            <h2 className="pl-5">About Planet:</h2>
+            <div>
+              <p className="pl-5"><strong className="pr-5">Tempature:</strong>{planet.climate}</p>
+              <p className="pl-5"><strong className="pr-5">Population:</strong>{planet.population}</p>
+            </div>
+            <p className="pl-5 pt-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis possimus repellat dicta numquam itaque, ex sint iste autem? Magnam voluptas accusamus distinctio tempora necessitatibus unde doloremque expedita praesentium itaque quisquam! Culpa voluptas dolorum exercitationem? Aliquid, blanditiis laudantium natus, voluptates quisquam at eius enim, debitis repellendus nesciunt veniam incidunt atque. Ipsum.
+            </p>
+          </div>
+        </div>
+        <p></p>
+
+      </div>
+    </>
+  )
 }
 
 export default ReadMorePlanets;
